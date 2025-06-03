8d56dc2ed4eb68b558683c5f20444d98ed02ba2d
ToluwaDavid <32324558+ToluwaDavid@users.noreply.github.com>
2025-06-03 18:35:48 +0100

    Update serve 001r.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json());
 
 app.use(cookieParser());
-CLIENT_URL = https://kaatin.vercel.app/
+CLIENT_URL = "https://kaatin.vercel.app/"
 const allowedOrigins = [process.env.DEV_URL, process.env.CLIENT_URL, CLIENT_URL];
 //Setting up CORS
 app.use(
